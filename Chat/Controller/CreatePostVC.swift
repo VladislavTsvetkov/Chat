31fec8861d2fb4abd44b27490f2b450131a47ee6
Владислав Цветков <>
2019-02-08 00:27:42 +0300

    реализована отправка сообщений в feed [138]

---
+++ Chat/Controller/CreatePostVC.swift
@@ -0,0 +1,61 @@
+//
+//  CreatePostVC.swift
+//  Chat
+//
+//  Created by Владислав Цветков on 07/02/2019.
+//  Copyright © 2019 Владислав Цветков. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class CreatePostVC: UIViewController {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var sendBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        textView.delegate = self
+    }
+    
+    @objc func closeKeyboard() {
+        view.endEditing(true)
+    }
+    
+    @IBAction func sendBtnWasPressed(_ sender: Any) {
+        if textView.text != nil && textView.text != "Say something here..." {
+            sendBtn.isEnabled = false
+            DataService.instance.uploadPost(withMessage: textView.text, forUID: (Auth.auth().currentUser?.uid)!, withGroupKey: nil) { (isComplete) in
+                if isComplete {
+                    self.sendBtn.isEnabled = true
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    self.sendBtn.isEnabled = false
+                    print("There is an error!")
+                }
+            }
+        }
+    }
+    
+    @IBAction func closeBtnWasPressed(_ sender: Any) {
+        view.endEditing(true)
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension CreatePostVC: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if (text == "\n") {
+            textView.resignFirstResponder()
+        }
+        return true
+    }
+}
---
+++ Chat/Controller/MeVC.swift
@@ -0,0 +1,28 @@
+//
+//  MeVC.swift
+//  Chat
+//
+//  Created by Владислав Цветков on 07/02/2019.
+//  Copyright © 2019 Владислав Цветков. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+        
+    }
+
+}
